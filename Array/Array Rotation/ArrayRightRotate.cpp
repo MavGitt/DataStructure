d949ba8fdde607a580f6be853572bb122b9bb466
Satyam Kumar <mavsatyamsingh@gmail.com>
2020-08-13 02:40:46 +0530

    rotation folder being added to array folder

---
+++ Array/Array Rotation/ArrayRightRotate.cpp
@@ -0,0 +1,58 @@
+/**
+ *    author:  MavricK_
+ *    created: 13-08-2020   
+**/
+#include <bits/stdc++.h>
+using namespace std;
+#define endl "\n"
+
+//function to reverse arr[] from index start to end
+void reverseArray(int arr[], int start, int end)
+{
+	while(start<end)
+	{
+	   swap(arr[start],arr[end]);
+	   start++;
+	   end--;
+	}
+}
+
+//function to right rotate array[] of size n by d
+void rightRotate(int arr[], int d , int n)
+{
+	if(d == 0)
+	    return;
+	reverseArray(arr, 0, n - 1);
+	reverseArray(arr, 0, d - 1);
+	reverseArray(arr, d, n - 1);
+
+}
+
+//function to display the array[]
+void display(int* a, int length)
+{
+    for (int i = 0; i < length; ++i)
+        cout << a[i] << " ";
+
+    cout << endl;
+}
+
+//driver code
+int main()
+{
+  int arr[] = {1,2,3,4,5,6,7};
+  int n = sizeof(arr)/sizeof(arr[0]);
+  int k = 2;
+  
+   // in case the rotating factor is 
+  // greater than array length 
+  k = k % n;
+  
+  //Function calling
+  rightRotate(arr, k, n);
+  display(arr, n);
+  
+  
+}
+
+
---
+++ Array/Array Rotation/Welcome file.md
@@ -0,0 +1,34 @@
+﻿# **The Reversal Algorithm**
+
+The Reversal algorithm is used for rotating the array in **K** places.
+
+Array Rotation can take place in **Two** ways.
+  - Left Rotation
+ - Right Rotation
+ ## Left Rotation
+
+`Input :  arr[] = [1, 2, 3, 4, 5, 6, 7] `
+                `d = 2`
+`Output : arr[] = [3, 4, 5, 6, 7, 1, 2]`
+
+ ## Right Rotation
+
+`Input :  arr[] = [1, 2, 3, 4, 5, 6, 7] `
+                `d = 2`
+`Output : arr[] = [6, 7, 1, 2, 3, 4, 5]`
+
+## Algorithm
+For Left Rotation
+ 1.  reverse(arr[], 0, d-1) ;
+ 2.  reverse(arr[], d , n-1);
+ 3.  reverse(arr[], 0, n-1);
+
+For Right Rotation
+ 1. reverse(arr[], 0, n-1);
+ 2. reverse(arr[], 0, d-1) ;
+ 3.  reverse(arr[], d , n-1);
+
+## Complexity
+`Time Complexity : O(n)`
+`Space Complexity : O(1)`
+   
